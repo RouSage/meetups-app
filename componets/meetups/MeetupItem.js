7040deb466b603d821aeb94da7f98c745b3b544e
RouSage <rousage@protonmail.com>
2021-06-22 16:31:24 +0300

    chore: basic project setup

---
+++ componets/meetups/MeetupItem.js
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import React from 'react';
+import Card from '../ui/Card';
+import classes from './MeetupItem.module.css';
+
+const MeetupItem = ({ image, title, address }) => {
+  return (
+    <li className={classes.item}>
+      <Card>
+        <div className={classes.image}>
+          <Image src={image} alt={title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{title}</h3>
+          <address>{address}</address>
+        </div>
+        <div className={classes.actions}>
+          <button>Show Details</button>
+        </div>
+      </Card>
+    </li>
+  );
+};
+
+export default MeetupItem;
