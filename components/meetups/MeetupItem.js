e494cbaee9403fc52e08dadd979fe6471b9e90ce
RouSage <rousage@protonmail.com>
2021-06-23 15:30:53 +0300

    feat: wrap components in Layout

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -8,7 +8,7 @@ const MeetupItem = ({ image, title, address }) => {
     <li className={classes.item}>
       <Card>
         <div className={classes.image}>
-          <Image src={image} alt={title} />
+          <Image src={image} alt={title} width={640} height={300} />
         </div>
         <div className={classes.content}>
           <h3>{title}</h3>
