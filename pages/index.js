8c7f4bf3460aa8c5411c7e0a6a5518d4cddec23a
RouSage <therousage@gmail.com>
2021-06-28 15:23:31 +0300

    feat: fetch meetups from API

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { MongoClient } from 'mongodb';
 import React from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
@@ -26,9 +27,25 @@ const HomePage = ({ meetups }) => {
 
 export const getStaticProps = async () => {
   // fetch data from an API
+
+  const client = await MongoClient.connect(
+    'mongodb+srv://dbUser:sRZy0FvVPoNaRP69@cluster0.qw4dx.mongodb.net/meetupsDB?retryWrites=true&w=majority'
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      meetups: MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        id: meetup._id.toString(),
+        title: meetup.title,
+        image: meetup.image,
+        address: meetup.address,
+      })),
     },
     revalidate: 1,
   };
