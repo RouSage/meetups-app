cd0f8b6c507075e0cd870b73536d80fbb5869dbf
RouSage <therousage@gmail.com>
2021-06-28 15:23:55 +0300

    refactor: remove mock meetups

--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,6 @@ import { MongoClient } from 'mongodb';
 import React from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
-const MEETUPS = [
-  {
-    id: 'm1',
-    title: 'First Meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Venezia_Palazzo_Ducale_01.jpg/800px-Venezia_Palazzo_Ducale_01.jpg',
-    address: 'Some Address 5, 12345 Some City',
-    description: 'This is a first meetup!',
-  },
-  {
-    id: 'm2',
-    title: 'Second Meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Venezia_Palazzo_Ducale_01.jpg/800px-Venezia_Palazzo_Ducale_01.jpg',
-    address: 'Some Address 20, 12653 Some City',
-    description: 'This is a second meetup!',
-  },
-];
-
 const HomePage = ({ meetups }) => {
   return <MeetupList meetups={meetups} />;
 };
